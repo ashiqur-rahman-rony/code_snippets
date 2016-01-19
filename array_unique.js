d66b5492ac1fdd2f373fcf1901bf3ec6071d996a
Ashiqur Rahman <ashiquebd@gmail.com>
2016-01-20 00:40:44 +0600

    Create array_unique.js

---
+++ array_unique.js
@@ -0,0 +1,11 @@
+/**
+ * A JavaScript code snippet to remove duplicates from array.
+ * The array will have unique items after the iteration.
+ **/
+var arr = [1, 3, 2, 4, 3, 5, 2, 9, 1]; 
+
+arr.reverse().filter(function (e, i, arr) {
+    return arr.indexOf(e, i+1) === -1;
+}).reverse();
+
+console.log(arr);
